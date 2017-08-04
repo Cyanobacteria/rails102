0916be9cb0debdc734cb08387bfebe7714761311
cyanobacteria <vodomelkor@gmail.com>
2017-08-05 03:13:36 +0800

    create groups index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get '/hihi', :to => 'hihi#hihi' 
-
-
+root 'groups#index'
+ resources  :groups
 end
