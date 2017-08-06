6f08ceae75ee62bb816cf54ae77a7e6e428ce508
cyanobacteria <vodomelkor@gmail.com>
2017-08-06 18:09:33 +0800

    已經完成crud 接著要簡化程式碼 與 學習view裡面的東西form的細節

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,56 @@
 class GroupsController < ApplicationController
-
+before_action :set_params, :only => [:show, :edit, :update, :destroy]
   def index 
     @groups = Group.all
   end
+
+  def new
+    @group = Group.new
+  end 
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      
+      redirect_to groups_path
+    else
+      redirect_to :new
+    end
+  end
+  def edit
+
+  end  
+  
+  def show
+
+  end
+   
+  def update
+    @group.update(group_params)
+    redirect_to groups_path, notice: "Update Success"
+  end
+
+  def destroy
+    @group.destroy
+
+    redirect_to groups_path    
+
+  end
+
+
+
+ private
+
+    def set_params
+      @group = Group.find(params[:id])
+    end
+
+    def group_params
+      params.require(:group).permit(:title, :description)
+    end
+
+
+
+
 
 end
