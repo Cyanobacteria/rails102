044230ed32e463e36feae81d425bf561f07b0fc4
cyanobacteria <vodomelkor@gmail.com>
2017-08-17 12:49:49 +0800

    implement posts pagination with will_paginate

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -28,7 +28,7 @@ before_action :checkout_permission, only: [:edit, :update, :destroy]
   
   def show
     @group = Group.find(params[:id])
-    @posts = @group.posts.recent
+    @posts = @group.posts.recent.paginate(:page => params[:page], :per_page = > 5)
   end
    
   def update
