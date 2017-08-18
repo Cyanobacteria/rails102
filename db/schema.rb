3db2742c4b3d7081a63130fccd3aa53e9826cce7
cyanobacteria <vodomelkor@gmail.com>
2017-08-18 17:43:40 +0800

    implement group ralation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817043048) do
+ActiveRecord::Schema.define(version: 20170818094127) do
+
+  create_table "group_relationships", force: :cascade do |t|
+    t.integer "group_id"
+    t.integer "usr_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string "title"
